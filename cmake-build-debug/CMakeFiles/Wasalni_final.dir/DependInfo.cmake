269fe111a948214e211d5437f70a1826d352be61
Mohamed Salah <mosalah3480@gmail.com>
2022-05-30 23:26:11 +0200

    cmake_create

---
+++ cmake-build-debug/CMakeFiles/Wasalni_final.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/Colors.cpp" "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/cmake-build-debug/CMakeFiles/Wasalni_final.dir/Colors.cpp.o"
+  "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/FileHandling.cpp" "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/cmake-build-debug/CMakeFiles/Wasalni_final.dir/FileHandling.cpp.o"
+  "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/Graph.cpp" "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/cmake-build-debug/CMakeFiles/Wasalni_final.dir/Graph.cpp.o"
+  "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/StartMenu.cpp" "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/cmake-build-debug/CMakeFiles/Wasalni_final.dir/StartMenu.cpp.o"
+  "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/Validation.cpp" "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/cmake-build-debug/CMakeFiles/Wasalni_final.dir/Validation.cpp.o"
+  "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/edge.cpp" "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/cmake-build-debug/CMakeFiles/Wasalni_final.dir/edge.cpp.o"
+  "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/main.cpp" "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/cmake-build-debug/CMakeFiles/Wasalni_final.dir/main.cpp.o"
+  "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/shortestPath.cpp" "/media/itsmosalah/3CC47A79C47A34EC/myprojects/ds/vs_wsalni/Wasalni_final/cmake-build-debug/CMakeFiles/Wasalni_final.dir/shortestPath.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Wasalni_final.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Wasalni_final.
+# This may be replaced when dependencies are built.
